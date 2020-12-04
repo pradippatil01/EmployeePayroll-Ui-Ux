d4145fb2cd9ab3cfead8ce2834bd08e1597cf382
Kamini Patil <kamini.patil@ad.infosys.com>
2020-12-04 15:56:15 +0530

    Added: Added regex to valided name

--- js/employee.js
+++ js/employee.js
@@ -1,4 +1,4 @@
-
+/*
 // salary event
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
@@ -6,17 +6,28 @@ output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
-
-function save() {
+*/
+function submit() {
     let name = document.getElementById('name').value;
-    let gender = document.getElementsByName('gender').value;
-    let department = document.getElementById('city').value;
-    let day = document.getElementById('day').value;
-    let month = document.getElementById('month').value;
-    let year = document.getElementById('year').value;
-    let salary = document.getElementById('salary').value;
-    let notes = document.getElementById('notes').value;
-
     alert(name);
-  
+}
+
+function cancel() {
+
+}
+
+function reset() {
+
+}
+
+function validate() {
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if (nameRegex.test(text.value)) {
+        textError.textContent = "";
+    }
+    else {
+        textError.textContent = "Name is incorrect";
+    }
 }
