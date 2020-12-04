f3067a16159f145bcd1e74853bc0d7beae84284e
Kamini Patil <kamini.patil@ad.infosys.com>
2020-12-04 16:48:55 +0530

    Refractor : Refractored validate function disapare error msg afte empty input

--- js/employee.js
+++ js/employee.js
@@ -1,12 +1,4 @@
-/*
-// salary event
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
-    output.textContent = salary.value;
-});
-*/
+
 function submit() {
     let name = document.getElementById('name').value;
     alert(name);
@@ -21,10 +13,13 @@ function reset() {
 }
 
 function validate() {
-    const text = document.querySelector('#name');
+    let name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if (nameRegex.test(text.value)) {
+    if (nameRegex.test(name.value)) {
+        textError.textContent = "";
+    }
+    else if (name.value.length == 0) {
         textError.textContent = "";
     }
     else {
